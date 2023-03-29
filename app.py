01d56bf478c5822a896f5ae6e7d5b9a93b2e8b31
zpc <zip_city@163.com>
2023-03-29 19:08:00 +0800

    update

--- app.py
+++ app.py
@@ -1,30 +1,37 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, redirect
 import os
 
 app = Flask(__name__)
 
-@app.route('/', defaults = {'path':'.'})
-@app.route('/<path:path>/')
+@app.route('/', defaults = {'path':'./'})
+@app.route('/<path:path>')
 def direct(path):
+    if path.endswith('/'):
+        if os.path.isdir(path):
+            dirs = os.listdir(path)
+            print(dirs)                 
+            hrefs = []
+            for f in dirs:
+                if f == 'index.html':
+                    with open(path+f) as page:
+                        contents = page.read()
+                    return contents
+                if os.path.isdir(path+f):
+                    hrefs.append(f+'/')
+                else:
+                    hrefs.append(f)
+            return render_template("directfor.html", dirs = hrefs)
+        return render_template('404.html'), 404
     if os.path.isdir(path):
-        path += '/'
-        dirs = os.listdir(path)                 
-        navi = []
-        for f in dirs:
-            if f == 'index.html':
-                with open(path+f) as page:
-                    contents = page.read()
-                return contents
-            if os.path.isdir(path+f):
-                navi.append(f+'/')
-            else:
-                navi.append(f)            
-        return render_template("directfor.html", dirs = navi)
-    elif path.endswith('.html'):
-        with open(path) as f:
-            contents = f.read()
-        return contents
-    return send_file(path, as_attachment=True)
-    
+        return redirect(path.split('/')[-1]+'/')
+    if os.path.isfile(path):
+        if path.endswith('html'):
+            with open(path) as f:
+                contents = f.read()
+            return contents
+        return send_file(path, as_attachment=True)
+    return render_template('404.html'), 404
+
+  
 if __name__ == "__main__":
-    app.run(debug = True)
+    app.run(debug = True, port=5000)
