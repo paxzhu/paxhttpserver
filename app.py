7d1c243b476e425b79354b0dae1fad404a845796
zpc <zip_city@163.com>
2023-03-30 01:48:16 +0800

    update

--- app.py
+++ app.py
@@ -7,30 +7,28 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def direct(path):
     if path.endswith('/'):
-        if os.path.isdir(path):
-            dirs = os.listdir(path)
-            print(dirs)                 
-            hrefs = []
-            for f in dirs:
-                if f == 'index.html':
-                    with open(path+f) as page:
-                        contents = page.read()
-                    return contents
-                if os.path.isdir(path+f):
-                    hrefs.append(f+'/')
-                else:
-                    hrefs.append(f)
-            return render_template("directfor.html", dirs = hrefs)
-        return render_template('404.html'), 404
+        if not os.path.isdir(path):
+            return render_template('404.html'), 404
+        subs = os.listdir(path)
+        print(subs)         
+        if 'index.html' in subs:
+            with open(path+f) as page:
+                contents = page.read()
+            return contents
+        hrefs = [f+'/' if os.path.isdir(path+f) else f for f in subs]
+        return render_template("directfor.html", cur_dir = path, subs = hrefs)
+        
     if os.path.isdir(path):
         return redirect(path.split('/')[-1]+'/')
-    if os.path.isfile(path):
-        if path.endswith('html'):
-            with open(path) as f:
-                contents = f.read()
-            return contents
-        return send_file(path, as_attachment=True)
-    return render_template('404.html'), 404
+    
+    if not os.path.isfile(path):
+        return render_template('404.html'), 404
+    if path.endswith('html'):
+        with open(path) as f:
+            contents = f.read()
+        return contents
+    return send_file(path, as_attachment=True)
+    
 
   
 if __name__ == "__main__":
