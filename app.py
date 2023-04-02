e90c401fc9a2f4a107063b1f25ad07c798f7746b
zpc <zip_city@163.com>
2023-04-03 00:24:22 +0800

    Updated the description of the cur directory

--- app.py
+++ app.py
@@ -3,35 +3,39 @@ import os
 
 app = Flask(__name__)
 
-def handle_dir(path: str) -> str:
+def handle_path(path: str) -> str:
+    if path == './':
+        return path
     stack = []
     path = path.split('/')
-    print(path)
+    # print(path)
     for sub in path:
         if sub == '..':
             if stack:
                 stack.pop()
         elif sub != '.' and sub != '':
             stack.append(sub)
-    return '/'.join(stack)
+    preview = '/'.join(stack)
+    print(f'preview:{preview}')
+    return preview
 
 @app.route('/', defaults = {'path':'./'})
 @app.route('/<path:path>')
 def direct(path):
-    # path = handle_dir(path)
-    path = os.path.realpath(path)
-    print(path)
     if path.endswith('/'):
         if not os.path.isdir(path):
             return render_template('404.html'), 404
         subs = os.listdir(path)
-        print(subs)         
+        # print(subs)         
         if 'index.html' in subs:
-            with open(path+f) as page:
+            with open(path+'index.html') as page:
                 contents = page.read()
             return contents
         hrefs = [f+'/' if os.path.isdir(path+f) else f for f in subs]
-        return render_template("directfor.html", cur_dir = path, subs = hrefs)
+        cur_dir = '/'
+        if path != './':
+            cur_dir += path
+        return render_template("directfor.html", cur_dir = cur_dir, subs = hrefs)
         
     if os.path.isdir(path):
         return redirect(path.split('/')[-1]+'/')
@@ -43,8 +47,6 @@ def direct(path):
             contents = f.read()
         return contents
     return send_file(path, as_attachment=True)
-    
 
-  
 if __name__ == "__main__":
     app.run(debug = True, port=5000)
