8440a1b36142e31e50019a313d90e8ecf3859ee8
zpc <zip_city@163.com>
2023-04-09 03:08:38 +0800

    Add functionality: Ability to set a specified system folder as the current working directory

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, send_file, redirect
 import os
-
+import sys
+"""
+CWD: current working directory
+"""
 app = Flask(__name__)
 def simplify_logic_path(url_path):
     stack = []
@@ -13,10 +16,10 @@ def simplify_logic_path(url_path):
         stack.append('')
     return '/'.join(stack)
 
-def path_exists(url_path):
-    if url_path.endswith('/') and not os.path.isdir(url_path):
+def path_exists(fs_path):
+    if fs_path.endswith('/') and not os.path.isdir(fs_path):
         return False
-    if not url_path.endswith('/') and not os.path.isfile(url_path):
+    if not fs_path.endswith('/') and not os.path.isfile(fs_path):
         return False
     return True
 
@@ -37,46 +40,53 @@ def preprocess(url_path):
     # print(simplified)
     if simplified != url_path:
         return redirect(simplified, code = 301)
-    if os.path.isdir(url_path) and not url_path.endswith('/'):
+    
+    fs_path = os.path.join(CWD, url_path)
+
+    if os.path.isdir(fs_path) and not fs_path.endswith('/'):
         # print(url_path+'/')
         return redirect('/'+ url_path + '/', code = 301) 
-    if url_path and not path_exists(url_path):
+    
+    if not path_exists(fs_path):
         return render_template('404.html'), 404
 
-def handle_directory(url_path):
-    temp = url_path
-    if not url_path:
-        temp = '.'
-    subs = [sub.name+'/' if sub.is_dir() else sub.name for sub in os.scandir(temp)]
+def handle_directory(url_path, fs_path):
+    subs = [sub.name+'/' if sub.is_dir() else sub.name for sub in os.scandir(fs_path)]
     subs.append('../')
     subs.sort()
     if 'index.html' in subs:
-        with open(os.path.join(url_path, 'index.html')) as page:
+        with open(os.path.join(fs_path, 'index.html')) as page:
             contents = page.read()
         return contents
     return render_template('directfor.html', cur_dir = '/' + url_path, subs = subs)
 
-def handle_file(url_path):
-    with open(url_path) as page:
+def handle_file(fs_path):
+    with open(fs_path) as page:
         contents = page.read()
     return contents
 
 @app.route('/', defaults = {'url_path':''})
 @app.route('/<path:url_path>')
 def direct(url_path):
-    print(url_path)
     """
     There may be a problem with user input, which will cause an exception, so preprocessing is required
     """
     handle = preprocess(url_path) 
     if handle:
         return handle
-
-    """Now, url_path is the normal path"""
+    """
+    Now, url_path is the normal path
+    Expose a URL path to the outside, and use the file system path to check for existence and retrieve data.
+    """
+    fs_path = os.path.join(CWD, url_path)
     
-    if not url_path or url_path.endswith('/'):
-        return handle_directory(url_path)
-    return handle_file(url_path)
+    if fs_path.endswith('/'):
+        return handle_directory(url_path, fs_path)
+    return handle_file(fs_path)
 
 if __name__ == "__main__":
+    CWD = '.'
+    if len(sys.argv) > 1:
+        CWD = sys.argv[1]
+    print(sys.argv)
     app.run(debug = True, port=9000)
